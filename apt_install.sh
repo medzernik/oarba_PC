557d1162a9196890a1ea260125ea8f3ddfab6543
medzernik <medzernik1@gmail.com>
2018-08-29 20:32:35 +0200

    Update apt_install.sh

--- apt_install.sh
+++ apt_install.sh
@@ -1,10 +1,10 @@
 #/bin/bash
 
-pkexec add-apt-repository ppa:libreoffice/ppa
+add-apt-repository ppa:libreoffice/ppa
 sudo add-apt-repository ppa:danielrichter2007/grub-customizer
-pkexec apt update
-pkexec apt dist-upgrade
+apt update
+apt dist-upgrade
 
-pkexec apt install -y wine-stable ttf-mscorefonts-installer grub-customizer `check-language-support -l sk` libreoffice 
+apt install -y wine-stable ttf-mscorefonts-installer grub-customizer `check-language-support -l sk` libreoffice 
 
 cat configkde.txt >> .config/kdeglobals
